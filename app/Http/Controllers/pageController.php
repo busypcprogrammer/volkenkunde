89319beb472f851a850e9f9283bd480f58d07369
Danny van Tol <danny.van.tol@hotmail.com>
2016-04-06 08:16:12 +0200

    Database changes and controller functions added

--- app/Http/Controllers/pageController.php
+++ app/Http/Controllers/pageController.php
@@ -10,15 +10,30 @@ class pageController extends Controller
 {
     public function index()
     {
+        echo self::navigationTracker();
+        $posts = \DB::table('post')
+                ->select('title')
+                ->addSelect('content')
+                ->addSelect('thumbnail')
+                ->addSelect('url')
+                ->whereRaw('uploaddate < NOW()')
+                ->orderBy('uploaddate', 'DESC')
+                ->limit(5)
+                ->get();
+        $postListing = '';
+        foreach($posts as $post)
+        {
+            $postListing .= '<div>' . $post->content . '</div>';
+        }
+        echo $postListing;
+    }
+    
+    public function categoryHandler($parent, $page = 0)
+    {
         
     }
     
-    public function parentHandler($parent, $page = 0)
-    {
-
-    }
-    
-    public function childHandler($parent, $child, $page = 0)
+    public function subCategoryHandler($parent, $child, $page = 0)
     {
         
     }
@@ -28,8 +43,41 @@ class pageController extends Controller
         
     }
     
-    private function navigationTracker()
+    private static function navigationTracker()
     {
-        
+        $navItems = \DB::table('navigation')
+                ->select('category.name')
+                ->addSelect('category.url')
+                ->addSelect('category.id')
+                ->where('navigation.parentid', '=', '0')
+                ->join('category', 'category.id', '=', 'navigation.categoryid')
+                ->orderBy('order')
+                ->get();
+        $nav = '';
+        foreach($navItems as $item)
+        {
+            $myString = '<ul>';
+            $subNavItems = \DB::table('navigation')
+                    ->select('category.name')
+                    ->addSelect('category.url')
+                    ->where('navigation.parentid', '=', $item->id)
+                    ->orderBy('order')
+                    ->join('category', 'category.id', '=', 'navigation.categoryid')
+                    ->get();
+            $myString .= '<li>' . $item->name . '</li>';
+            if (count($subNavItems) > 0)
+            {
+                $myString .= '<ul>';
+                foreach($subNavItems as $subItem)
+                {
+                    $myString .= '<li>' . $subItem->name . '</li>';
+                }
+                $myString .= '</ul>';
+            }
+            
+            $myString .= '</ul>';
+            $nav .= $myString;
+        }
+        return $nav;
     }
 }
--- database/migrations/2016_03_31_113540_post.php
+++ database/migrations/2016_03_31_113540_post.php
@@ -19,7 +19,7 @@ class Post extends Migration
             $table->text('keywords');
             $table->text('content');
             $table->string('url');
-            $table->tinyInteger('hidden');
+            $table->string('thumbnail');
             $table->timestamp('uploaddate');
         });
     }
